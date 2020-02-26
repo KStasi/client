c038e1c34a0e88cb3fd283b1c01cdb046ed0402f
kstasi <nastasiakondaurova@gmail.com>
2020-02-26 19:14:31 +0200

    errors handlers implemented

--- src/main/java/ua/akondaur/client/GoalController.java
+++ src/main/java/ua/akondaur/client/GoalController.java
@@ -1,7 +1,6 @@
 package ua.akondaur.client;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.validation.Valid;
@@ -9,23 +8,25 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 
 import ua.akondaur.db.GoalRepository;
 import ua.akondaur.db.SequenceGeneratorService;
 import ua.akondaur.client.ResourceNotFoundException;
 import ua.akondaur.db.Goal;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RestController
 class GoalController {
@@ -37,6 +38,24 @@ class GoalController {
 
 	@Value("${eureka.instance.instanceId}")
 	private Integer instanceId;
+
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public Map handleMessageException() {
+		Map result = new HashMap();
+
+		result.put("instanceId", instanceId);
+		result.put("content", "Bad request body");
+		return result;
+	}
+
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	public Map handleRequestException() {
+		Map result = new HashMap();
+
+		result.put("instanceId", instanceId);
+		result.put("content", "Method is not supported");
+		return result;
+	}
 
 	@GetMapping("/goals")
 	public Map getAllGoals() {
@@ -62,39 +81,59 @@ class GoalController {
 	}
 
 	@PostMapping("/goals")
-	public Goal createGoal(@Valid @RequestBody Goal goal) {
+	public Map createGoal(@Valid @RequestBody Goal goal) {
+		Map result = new HashMap();
+		result.put("instanceId", instanceId);
 		goal.id = sequenceGeneratorService.generateSequence(Goal.SEQUENCE_NAME);
-		return goalRepository.save(goal);
+		try {
+			result.put("content", goalRepository.save(goal));
+		} catch (Exception e) {
+			result.put("content", "Goal cannot be created");
+		}
+		return result;
 	}
 
 	@PutMapping("/goals/{id}")
-	public ResponseEntity<Goal> updateGoal(@PathVariable(value = "id") Long id, @Valid @RequestBody Goal goalDetails)
-			throws ResourceNotFoundException {
-		Goal goal = goalRepository.findById(id)
-				.orElseThrow(() -> new ResourceNotFoundException("Goal not found for this id :: " + id));
+	public Map updateGoal(@PathVariable(value = "id") Long id, @Valid @RequestBody Goal goalDetails) {
+		Map result = new HashMap();
+		result.put("instanceId", instanceId);
+		try {
+			Goal goal = goalRepository.findById(id)
+					.orElseThrow(() -> new ResourceNotFoundException("Goal not found for this id :: " + id));
 
-		goal.topic = goalDetails.topic;
-		goal.author = goalDetails.author;
-		goal.tags = goalDetails.tags;
-		goal.time = goalDetails.time;
-		goal.description = goalDetails.description;
-		goal.resources = goalDetails.resources;
-		goal.reasons = goalDetails.reasons;
-		goal.measures = goalDetails.measures;
-		goal.achievements = goalDetails.achievements;
-		final Goal updatedGoal = goalRepository.save(goal);
-		return ResponseEntity.ok(updatedGoal);
+			goal.topic = goalDetails.topic;
+			goal.author = goalDetails.author;
+			goal.tags = goalDetails.tags;
+			goal.time = goalDetails.time;
+			goal.description = goalDetails.description;
+			goal.resources = goalDetails.resources;
+			goal.reasons = goalDetails.reasons;
+			goal.measures = goalDetails.measures;
+			goal.achievements = goalDetails.achievements;
+
+			result.put("content", ResponseEntity.ok(goalRepository.save(goal)));
+		} catch (Exception e) {
+			result.put("content", "Goal cannot be updated");
+		}
+		return result;
 	}
 
 	@DeleteMapping("/goals/{id}")
-	public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") Long id) throws ResourceNotFoundException {
-		Goal goal = goalRepository.findById(id)
-				.orElseThrow(() -> new ResourceNotFoundException("Goal not found for this id :: " + id));
+	public Map deleteEmployee(@PathVariable(value = "id") Long id) {
+		Map result = new HashMap();
+		result.put("instanceId", instanceId);
+		try {
+			Goal goal = goalRepository.findById(id)
+					.orElseThrow(() -> new ResourceNotFoundException("Goal not found for this id :: " + id));
 
-		goalRepository.delete(goal);
-		Map<String, Boolean> response = new HashMap<>();
-		response.put("deleted", Boolean.TRUE);
-		return response;
+			goalRepository.delete(goal);
+			Map<String, Boolean> response = new HashMap<>();
+			response.put("deleted", Boolean.TRUE);
+			result.put("content", response);
+		} catch (Exception e) {
+			result.put("content", "Goal cannot be updated");
+		}
+		return result;
 	}
 
 }
